8663ede8a1d7fade48b89873f507e4aa400d01f7
Matthieu <matthieu.lienart@gmail.com>
2020-10-05 19:41:01 +0200

    added servers

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
-resource "aws_instance" "web" {
-  # ...
+module "server" {
+  source = "./server"
+
+  nb_instances = var.nb_instances
+
+  subnet_id    = var.environment1_subnet_id
+  server_name  = var.server_name
+  nsgs          = var.nsgs
+  key_name     = var.key_name
+  default_tags = var.default_tags
 }
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
